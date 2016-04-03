fd0306edd2f7dab884ff96c0b8fd49d22b159e3b
StefMa <StefMaDev@outlook.com>
2016-04-03 19:20:55 +0200

    3.08 Solution

---
+++ 3.08-Exercise-AddAFlavor-SpecificActivity/app/src/paid/java/com/udacity/gradle/flavorspecificactivity/JokeActivity.java
@@ -0,0 +1,15 @@
+package com.udacity.gradle.flavorspecificactivity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class JokeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_joke);
+    }
+}
